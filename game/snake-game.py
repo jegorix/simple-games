3f52720bb5b5bbdf5e3ae1b62b34831c01bf79e6
jegorix <0311egor@gmail.com>
2025-03-08 19:05:37 +0300

    add dropped rect

--- game/snake-game.py
+++ game/snake-game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -11,15 +12,23 @@ running = True
 # background_image = pygame.image.load('images/back.jpeg')
 # background_image = pygame.transform.scale(background_image, window_size)
 # screen.blit(background_image, (0, 0))
-rect_size = (100, 100)
-rect_color = (255, 255, 255)
 circle_radius = 20
 circle_color = (255, 0, 0)
 x_pos = window_size[0] // 2
 y_pos = window_size[1] // 2
-# x_pos = 0
-# y_pos = 0
 speed = 1
+
+rect_height = 50
+rect_width = 50
+rect_color = (0, 0, 255)
+rect_x = random.randint(0, window_size[0] - rect_width)
+rect_y = -rect_height
+rect_speed = 0.5
+
+
+score = 0
+
+
 
 while running:
     screen.fill(background_color)
@@ -38,8 +47,15 @@ while running:
         x_pos += speed
 
 
+    rect_y += rect_speed
+    if rect_y > window_size[1]:
+        rect_y = -rect_height
+        rect_x = random.randint(0, window_size[0] - rect_width)
+        score += 1
 
 
+
+    pygame.draw.rect(screen, rect_color, (rect_x, rect_y, rect_width, rect_height))
     pygame.draw.circle(screen, circle_color, (x_pos, y_pos), circle_radius)
     pygame.display.update()
 
