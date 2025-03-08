6640fa66f39d2fc981e564ae92813cca5667d70d
jegorix <0311egor@gmail.com>
2025-03-08 16:56:14 +0300

    quit fix

--- game/snake-game.py
+++ game/snake-game.py
@@ -7,10 +7,11 @@ window_size = (800, 600)
 screen = pygame.display.set_mode(window_size)
 background_color = (0, 0, 0)
 screen.fill(background_color)
+running = True
 
-
-while True:
+while running:
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            running = False
