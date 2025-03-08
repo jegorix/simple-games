38254924e679d7d9c89cce786a2c36f66b4900a3
jegorix <0311egor@gmail.com>
2025-03-08 17:15:05 +0300

    add image

--- game/snake-game.py
+++ game/snake-game.py
@@ -9,6 +9,11 @@ background_color = (0, 0, 0)
 screen.fill(background_color)
 running = True
 
+background_image = pygame.image.load('images/back.jpeg')
+background_image = pygame.transform.scale(background_image, window_size)
+screen.blit(background_image, (0, 0))
+
+
 while running:
     pygame.display.update()
     for event in pygame.event.get():
