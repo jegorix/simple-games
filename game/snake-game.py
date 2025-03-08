1c4d1e5b623868fb07bab6cd313d48c258fa0aa0
jegorix <0311egor@gmail.com>
2025-03-08 17:27:14 +0300

    identify size and position, draw rect

--- game/snake-game.py
+++ game/snake-game.py
@@ -3,18 +3,22 @@ import pygame
 pygame.init()
 
 window_size = (800, 600)
-
+pygame.display.set_caption("Project")
 screen = pygame.display.set_mode(window_size)
 background_color = (0, 0, 0)
-screen.fill(background_color)
 running = True
 
-background_image = pygame.image.load('images/back.jpeg')
-background_image = pygame.transform.scale(background_image, window_size)
-screen.blit(background_image, (0, 0))
-
+# background_image = pygame.image.load('images/back.jpeg')
+# background_image = pygame.transform.scale(background_image, window_size)
+# screen.blit(background_image, (0, 0))
+rect_size = (100, 100)
+rect_color = (255, 255, 255)
+x_pos = (window_size[0] - rect_size[0]) / 2
+y_pos = (window_size[1] - rect_size[1]) / 2
 
 while running:
+    screen.fill(background_color)
+    pygame.draw.rect(screen, rect_color, (x_pos, y_pos, rect_size[0], rect_size[1]))
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
