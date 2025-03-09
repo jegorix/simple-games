718360d4825377a33415b96f8c0cf50838a16c27
jegorix <0311egor@gmail.com>
2025-03-09 15:25:04 +0300

    load image

--- game/train.py
+++ game/train.py
@@ -6,6 +6,8 @@ screen = pygame.display.set_mode((800, 600))
 square = pygame.Surface((50,170))
 square.fill('Blue')
 
+background_image = pygame.image.load('images/back.jpeg')
+
 myfont = pygame.font.Font('fonts/PTSerif-BoldItalic.ttf', 40)
 text_surface = myfont.render("Jegorix", False, (255,255,255))
 
@@ -17,6 +19,8 @@ while running:
     pygame.draw.circle(screen, 'Red', (252, 85), 10 )
 
     screen.blit(text_surface, (300,100))
+
+    screen.blit(background_image, (550, 200))
 
     pygame.display.update()
     for event in pygame.event.get():
