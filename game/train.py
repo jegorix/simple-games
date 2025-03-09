4b2d6ed893870fe982b107b19712c30a7e86a8bd
jegorix <0311egor@gmail.com>
2025-03-09 15:22:14 +0300

    create fonts folder and render text

--- game/train.py
+++ game/train.py
@@ -6,12 +6,17 @@ screen = pygame.display.set_mode((800, 600))
 square = pygame.Surface((50,170))
 square.fill('Blue')
 
+myfont = pygame.font.Font('fonts/PTSerif-BoldItalic.ttf', 40)
+text_surface = myfont.render("Jegorix", False, (255,255,255))
+
 
 running = True
 while running:
     screen.blit(square, (0, 500 - square.get_height()))
 
-    pygame.draw.circle(screen, 'Red', (200, 200), 40 )
+    pygame.draw.circle(screen, 'Red', (252, 85), 10 )
+
+    screen.blit(text_surface, (300,100))
 
     pygame.display.update()
     for event in pygame.event.get():
