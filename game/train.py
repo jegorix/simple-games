7960b66c036637c1d0eb7901dfc20d8384ce5013
jegorix <0311egor@gmail.com>
2025-03-09 15:39:01 +0300

    draw rect

--- game/train.py
+++ game/train.py
@@ -7,6 +7,7 @@ screen = pygame.display.set_mode(window_size)
 square = pygame.Surface((50,170))
 square.fill('Blue')
 
+
 header_font = pygame.font.Font(None, 72)
 header_text = header_font.render("Press SPACE to start", True, (255,255,255))
 
@@ -22,6 +23,8 @@ while running:
     screen.blit(square, (0, 500 - square.get_height()))
 
     pygame.draw.circle(screen, 'Red', (252, 85), 10 )
+
+    pygame.draw.rect(screen, "Green", (5, 5, 50, 50))
 
     screen.blit(text_surface, (300,100))
 
