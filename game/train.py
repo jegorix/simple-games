f07572cebbd999ec1fccaf170e0927f1328c58b4
jegorix <0311egor@gmail.com>
2025-03-09 14:52:21 +0300

    draw square object

---
+++ game/train.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+
+square = pygame.Surface((50,170))
+square.fill('Blue')
+
+
+running = True
+while running:
+    screen.blit(square, (0, 500 - square.get_height()))
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            running = False
+
