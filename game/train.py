f074b9d0b2654c53b0992c3c1b445862f2ff7dbe
jegorix <0311egor@gmail.com>
2025-03-09 14:56:48 +0300

    draw circle object

--- game/train.py
+++ game/train.py
@@ -10,6 +10,9 @@ square.fill('Blue')
 running = True
 while running:
     screen.blit(square, (0, 500 - square.get_height()))
+
+    pygame.draw.circle(screen, 'Red', (200, 200), 40 )
+
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
