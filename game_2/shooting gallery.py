859c051bb681cc127b753459d6bda746916d0301
jegorix <0311egor@gmail.com>
2025-03-21 22:58:10 +0300

    add target

--- game_2/shooting gallery.py
+++ game_2/shooting gallery.py
@@ -1,21 +1,46 @@
 import pygame
+import random
 pygame.init()
 
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 900, 500
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Shooting Range: Target Hunting")
 
 clock = pygame.time.Clock()
 
 running = True
 
+class Target():
+    def __init__(self):
+        self.x = random.randint(100, WIDTH - 100)
+        self.y = random.randint(100, HEIGHT - 100)
+        self.radius = 30
+        self.color = (255, 0, 0)
+
+    def draw_target(self):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+
+
+targets = [Target() for _ in range(5)]
+
+
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
             running = False
 
+
     screen.fill((0, 0, 0))
+    for target in targets:
+        target.draw_target()
+
+
+
     pygame.display.flip()
     clock.tick(60)
+
+pygame.quit()
 
 
